50bfbcad23624a2b7e6b77d43573e3bf4bbd4ee7
Zanvis <legoo1312@gmail.com>
2024-06-04 21:43:18 +0200

    Added google maps preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ZipCodeComponent } from './zip-code/zip-code.component';
 import { GoogleMapsComponent } from './google-maps/google-maps.component';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +14,7 @@ import { CommonModule, isPlatformBrowser } from '@angular/common';
 export class AppComponent {
   title = 'ZipCodeApp';
   AllData: any;
+  
   setData(value : any) {
     this.AllData = value
   }
